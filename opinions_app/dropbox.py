f7af15407eb64c9392e2df8d4eb1a8bd1b5c2c0d
Ivan Sopenko <ivansopenko@gmail.com>
2025-09-21 21:40:07 +0300

    add async file upload

--- opinions_app/dropbox.py
+++ opinions_app/dropbox.py
@@ -1,6 +1,7 @@
+import asyncio
 import json
 
-import requests
+import aiohttp
 
 from . import app
 
@@ -12,35 +13,83 @@ SHARING_LINK = (
 )
 
 
-def upload_files_to_dropbox(images):
-    urls = []
+async def upload_file_and_get_url(session, image):
+    dropbox_args = json.dumps({
+        'mode': 'add',
+        'path': f'/{image.filename}',
+        'autorename': True,
+    })
+    async with session.post(
+        UPLOAD_LINK,
+        data=image.read(),
+        headers={
+            'Authorization': AUTH_HEADER,
+            'Content-Type': 'application/octet-stream',
+            'Dropbox-API-Arg': dropbox_args
+        }
+    ) as response:
+        data = await response.json()
+        path = data['path_lower']
+    async with session.post(
+        SHARING_LINK,
+        json={'path': path},
+        headers={
+            'Authorization': AUTH_HEADER,
+            'Content-Type': 'application/json',
+        }
+    ) as response:
+        data = await response.json()
+        if 'url' not in data:
+            data = data['error']['shared_link_already_exists']['metadata']
+        url = data['url']
+        url = url.replace('&dl=0', '&raw=1')
+    return url
+
+
+async def async_upload_files_to_dropbox(images):
     if images is not None:
-        for image in images:
-            dropbox_args = json.dumps(
-                {'path': f'/{image.filename}', 'autorename': True}
-            )
-            response = requests.post(
-                UPLOAD_LINK,
-                image.read(),
-                headers={
-                    'Authorization': AUTH_HEADER,
-                    'Content-Type': 'application/octet-stream',
-                    'Dropbox-API-Arg': dropbox_args
-                }
-            )
-            path = response.json()['path_lower']
-            response = requests.post(
-                SHARING_LINK,
-                json={'path': path},
-                headers={
-                    'Authorization': AUTH_HEADER,
-                    'Content-Type': 'application/json',
-                }
-            )
-            data = response.json()
-            if 'url' not in data:
-                data = data['error']['shared_link_already_exists']['metadata']
-            url = data['url']
-            url = url.replace('&dl=0', '&raw=1')
-            urls.append(url)
-    return urls
+        tasks = []
+        async with aiohttp.ClientSession() as session:
+            for image in images:
+                tasks.append(
+                    asyncio.ensure_future(
+                        upload_file_and_get_url(session, image)
+                    )
+                )
+            urls = await asyncio.gather(*tasks)
+        return urls
+
+
+# def upload_files_to_dropbox(images):
+#     """Sync version."""
+#     urls = []
+#     if images is not None:
+#         for image in images:
+#             dropbox_args = json.dumps(
+#                 {'path': f'/{image.filename}', 'autorename': True}
+#             )
+#             response = requests.post(
+#                 UPLOAD_LINK,
+#                 image.read(),
+#                 headers={
+#                     'Authorization': AUTH_HEADER,
+#                     'Content-Type': 'application/octet-stream',
+#                     'Dropbox-API-Arg': dropbox_args
+#                 }
+#             )
+#             path = response.json()['path_lower']
+#             response = requests.post(
+#                 SHARING_LINK,
+#                 json={'path': path},
+#                 headers={
+#                     'Authorization': AUTH_HEADER,
+#                     'Content-Type': 'application/json',
+#                 }
+#             )
+#             data = response.json()
+#             if 'url' not in data:
+#                 data = data['error']['shared_link_already_exists']['metadata']
+#             url = data['url']
+#             url = url.replace('&dl=0', '&raw=1')
+#             urls.append(url)
+#     return urls
--- opinions_app/views.py
+++ opinions_app/views.py
@@ -3,7 +3,7 @@ from random import randrange
 from flask import abort, flash, redirect, render_template, url_for
 
 from . import app, db
-from .dropbox import upload_files_to_dropbox
+from .dropbox import async_upload_files_to_dropbox
 from .forms import OpinionForm
 from .models import Opinion
 
@@ -31,14 +31,14 @@ def opinion_view(id):
 
 
 @app.route('/add', methods=['GET', 'POST'])
-def add_opinion_view():
+async def add_opinion_view():
     form = OpinionForm()
     if form.validate_on_submit():
         text = form.text.data
         if Opinion.query.filter_by(text=text).first() is not None:
             flash('Такое мнение уже было оставлено ранее!')
             return render_template('add_opinion.html', form=form)
-        urls = upload_files_to_dropbox(form.images.data)
+        urls = await async_upload_files_to_dropbox(form.images.data)
         opinion = Opinion(
             title=form.title.data,
             text=form.text.data,
